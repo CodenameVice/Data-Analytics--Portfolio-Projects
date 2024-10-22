d4bd978f6974a4d5b66a126e94ddd9880488df10
seek.theLOOPER <91304820+CodenameVice@users.noreply.github.com>
2024-10-22 10:32:19 -0400

    Update CovidSQL.sql

--- CovidSQL.sql
+++ CovidSQL.sql
@@ -1,3 +1,6 @@
+---- USING SQL TO QUERY COVID DEATHS AND VACCINATION STATISTICS
+
+
 -- Total Cases vs Total Deaths
 
 Select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
